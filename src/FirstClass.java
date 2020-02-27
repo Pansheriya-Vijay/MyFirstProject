08565b6dc66b04ed3bbcd0eba905cefe364a3b49
Vijay Pan <61212185+Pansheriya-Vijay@users.noreply.github.com>
2020-02-27 15:21:21 -0500

    change in firstclass

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello");
-		System.out.println("my name is vijay dinanath chauhan");
+		System.out.println("my name is selenium");
 		
 	}
 
